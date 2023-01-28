942b1092c8613b0f72d31d186b77f3981761b81e
fran sola <fmia1992@gmail.com>
2023-01-28 23:21:43 +0100

    proyecto terminado

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,60 @@
+import { useEffect, useState } from 'react'
 import './App.css'
+
+const FollowMouse = () => {
+  const [enable, setEnable] = useState(false)
+  const [position, setPosition] = useState({ x: 0, y: 0 })
+
+  useEffect(() => {
+    console.log('useEffect', { enable })
+    const handleMove = (e) => {
+      const { clientX, clientY } = e
+      setPosition({ x: clientX, y: clientY })
+    }
+
+    if (enable) {
+      window.addEventListener('pointermove', handleMove)
+    }
+
+    return () => {
+      window.removeEventListener('pointermove', handleMove)
+    }
+  }, [enable])
+
+  useEffect(() => {
+    document.body.classList.toggle('no-cursor', enable)
+
+    return () => document.body.classList.remove('no-cursor')
+  }, [enable])
+
+  return (
+    <>
+      <div style={{
+        position: 'absolute',
+        backgroundColor: 'rgba(0, 0, 0, 0.5)',
+        border: '1px solid #fff',
+        borderRadius: '50%',
+        opacity: 0.8,
+        pointerEvents: 'none',
+        left: -25,
+        top: -25,
+        width: 50,
+        height: 50,
+        transform: `translate(${position.x}px, ${position.y}px)`
+      }}
+      />
+      <button onClick={() => setEnable(!enable)}>
+        {enable ? 'Desactivar' : 'Activar'} seguir puntero
+      </button>
+    </>
+  )
+}
 
 function App () {
   return (
-    <h3>Proyecto 3</h3>
+    <main>
+      <FollowMouse />
+    </main>
   )
 }
 
